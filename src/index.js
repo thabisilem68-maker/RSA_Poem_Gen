850ff6201e9452a81dfa511a6b8539e5d7e20089
thabisilem68-maker <thabisilem68@gmail.com>
2026-02-04 17:52:59 +0200

    added basic form handling and add type writter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+  newTypewriter("#poem", {
+    strings: "Christian gentlemen a few",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
